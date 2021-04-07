b1d2b8689eee31749f1b8020d17d3f7f0e40e940
fabianfv <fabianfloresvadell@gmail.com>
2021-04-07 09:15:46 +0200

    adds a little bash script that help me to create the online version

---
+++ make-online-version.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+git checkout master
+git branch -D onlineVersion
+git checkout -b onlineVersion
+rm -rf node_modules .vscode
+rm styles.scss .gitignore package.json package-lock.json js/tests.js css/paleta-colores.css.map css/styles.css.map
+echo "Don't forget to change local links to CDN links"
+#git add .
+#git commit -m "adds the onlineVersion branch to set the GitHub page of this practice"
+#git push -f origin onlineVersion
